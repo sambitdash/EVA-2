cc6d046b398cb5f6967c214da596d9d55e3547f1
Sambit Kumar Dash <sambitdash@gmail.com>
2020-05-30 11:58:45 +0530

    Adding TD3

---
+++ Phase-2/Session-10/car.jl
@@ -0,0 +1,312 @@
+using FileIO
+using Blink
+using LinearAlgebra
+using OffsetArrays: no_offset_view
+using BSON: @load, @save
+using Images
+#using Zygote
+#using CUDAdrv
+#using CuArrays
+
+#include("td3.jl")
+
+struct Car
+    img::Matrix
+    gray::Matrix
+    function Car()
+        img = load("car.png")
+        gray = Gray.(img)
+        return new(img, gray)
+    end
+end
+
+struct CarState
+    h::Float32
+    w::Float32
+    loc::Tuple{Float32, Float32}
+    goal::Tuple{Float32, Float32}
+    ang::Float32 #Clockwise positive
+end
+
+function CarState(c::Car, loc=(0f0, 0f0), goal=(400f0, 400f0), ang=0f0)
+    h, w = size(c.img)
+    return CarState(h, w, loc, goal, ang)    
+end
+
+function dist2(s::CarState)
+    loc = get_curr_loc(s, (0, s.h)) 
+    v = s.goal .- loc
+    return dot(v, v)
+end
+
+function rotate(s::CarState, angle)
+    ang = s.ang + angle
+    while ang < -pi
+        ang += 2pi
+    end
+    while ang > pi
+        ang -= 2pi
+    end
+    return CarState(s.h, s.w, s.loc, s.goal, ang)
+end
+
+function get_curr_loc(s::CarState, v)
+    sth, cth, ox, oy = sin(s.ang), cos(s.ang), s.loc[1], s.loc[2]
+    return v[1]*cth - v[2]*sth + ox, v[1]*sth + v[2]*cth + oy
+end
+
+FWD_SPEED = 200f0                        # Pixels per sec
+BK_SPEED_FACTOR = 10f0                   # Ratio of forward and backward speed
+BK_SPEED  = FWD_SPEED / BK_SPEED_FACTOR  
+ACTION_EVAL_INTERVAL = 10f0/FWD_SPEED     # Average check every 10 fwd pixels
+ACTIONS = (:forward, :left, :right, :back)
+
+roadmap = load("maze.png")
+
+l_m, w_m = size(roadmap)
+println(l_m, " ", w_m)
+
+#w = Window()
+#loadfile(w, joinpath(@__DIR__, "file.html"))
+
+goal = (400f0, 400f0)
+bloc = (50f0, 50f0)
+
+episode = 0
+
+car = Car()
+cs  = CarState(car, bloc, goal, 0f0)
+
+last_reward = 0f0
+payout = 0f0
+ntime = 0
+episodes = Vector{Tuple{Int, Int, Float32, Float32}}()
+episode_state = :e
+
+inited = false
+tm = time()
+
+println(:none, " ", cs)
+
+function plotLine(f::Function, p1, p2)
+    x0, y0 = p1
+    x1, y1 = p2
+    dx =  abs(x1 - x0)
+    sx = x0 < x1 ? 1 : -1
+    dy = -abs(y1 - y0)
+    sy = y0 < y1 ? 1 : -1
+    err = dx + dy  # error value e_xy 
+    while true
+        f(x0, y0) && return true
+        x0 == x1 && y0 == y1 && break
+        e2 = 2*err;
+        if e2 >= dy 
+            err += dy # e_xy+e_x > 0 
+            x0 += sx
+        end
+        if e2 <= dx  # e_xy+e_y < 0
+            err += dx
+            y0 += sy
+        end
+    end
+    return false
+end
+
+function car_on_wall(c::Car, s::CarState)
+    h, w = size(c.img)
+    w2 = div(w, 2)
+    p = map([(-w2, 0), (w2, 0), (w2, h), (-w2, h)]) do v
+        loc = get_curr_loc(s, v)
+        return round(Int, loc[1]), round(Int, loc[2])
+    end
+
+    f = (x, y)->!roadmap[y, x]
+    
+    return (plotLine(f, p[1], p[2]) || plotLine(f, p[2], p[3]) ||
+            plotLine(f, p[3], p[4]) || plotLine(f, p[4], p[1])) 
+end
+
+function reward(ps::CarState, s::CarState, a)
+    global car
+    sensors = get_curr_sensors(car, s)
+    car_on_wall(car, s) && return (-0.9f0, false)
+    #any(sensor_on_wall, sensors) && return (-1f-2, false)
+    d, dp = dist2(s), dist2(ps)
+    d < 625f0 && return (1f0, true)
+    d < dp && return (25f-3, false)
+    return (-25f-3, false)
+end
+
+function signal(s::CarState)
+    global l_m, w_m
+    h = s.h
+    loc = get_curr_loc(s, (0, h)) 
+    v = s.goal .- loc
+
+    bkimg = zeros(Gray{N0f8}, (80, 80))
+    
+    loc1 = get_curr_loc(s, (0, div(h, 2)))
+
+    midi = round.(Int, loc1)
+
+    minx, miny, maxx, maxy = midi[1]-40, midi[2]-40, midi[1]+39, midi[2]+39
+
+    cminx, cminy, cmaxx, cmaxy =
+        max(minx, 1), max(miny, 1), min(maxx, w_m), min(maxy, l_m)
+
+    rdimg = @view roadmap[cminy:cmaxy, cminx:cmaxx]
+
+    bkimg[cminy-miny+1:cmaxy-miny+1, cminx-minx+1:cmaxx-minx+1] = rdimg
+    
+    ang = s.ang
+    ang < 0 && (ang += 2*pi)
+    carimg = no_offset_view(imrotate(car.gray, ang, 0.75f0))
+
+    h, w = size(carimg)
+
+    oy, ox = 40-div(h, 2), 40-div(w, 2)
+
+    for i=1:h
+        for j=1:w
+            if abs(carimg[i, j] - 0.75f0) > 0.01f0
+                bkimg[oy+i, ox+j] = carimg[i, j]
+            end
+        end
+    end
+    return bkimg
+end
+
+function eval_action()
+    global cs, car, w, bloc, goal, tm, episode, last_reward, payout, ntime
+    global episode_state
+
+    while !trained
+        if episode_state === :e
+            episode += 1
+            ntime = 1
+            cs = CarState(car, bloc, goal, 0f0)
+            last_reward = 0f0
+            payout = 0f0
+            @js w carview.drawp(0, $(bloc...), true)
+            episode_state = :b
+            continue
+        end
+
+        ntime += 1
+        action = ACTIONS[rand(1:4)]#ACTIONS[update!(dqn, last_reward, signal(cs), episode_state)]
+        println(action)
+        episode_state = :c
+        speed = action == :back ? -BK_SPEED : FWD_SPEED
+        tm1 = time()
+        dtm = (tm1 - tm)
+        println(dtm)
+        dtm > 100f-3 && (dtm = 100f-3)
+        d = speed*dtm
+        tm = tm1
+        println("distance: ", d)
+        ns = cs
+
+        action == :left  && (ns = left(ns))
+        action == :right && (ns = right(ns))
+        if action == :forward || action == :back
+            ns = forward(ns, d)
+        end
+
+        r, isterminal = reward(cs, ns, action)
+
+        payout = gamma*payout + r
+
+        #println("episode = $episode ", action, " ", ns, " ", d, " nmem:", dqn.memory.n,
+        #        " reward:", r, " payout: ", payout)
+
+        if isterminal
+            episode_state = :e
+            act = signal(ns)
+            #update!(dqn, r, act, episode_state)
+            push!(episodes, (episode, ntime, payout, r))
+            ntime = 0
+            yield()
+            continue
+        end
+
+        last_reward = r
+        cs = ns
+        ang = action == :left  ? -ANG_STEP :
+              action == :right ?  ANG_STEP : 0
+        yd  = (action == :forward || action == :back) ? d : 0
+        println(ang, " ",  yd)
+        @js w carview.drawp($ang, 0, $yd)
+    end
+end
+
+#=
+tsk_eval  = @task eval_action()
+tsk_wait  = @task begin
+    v = read(stdin, 1)
+    dqn.trained = true
+    weights = params(dqn.model);
+    @save "model-weights.bson" weights
+end
+
+schedule(tsk_eval)
+schedule(tsk_wait)
+
+wait(tsk_wait)
+=#
+
+using Flux
+
+model = Chain(
+    #=
+    # 80x80 image
+    Conv((3, 3),  1=>16, pad=(1, 1)),
+    BatchNorm(16, relu),
+    MaxPool((2,2)),
+    # 40x40 image
+    Conv((3, 3), 16=>32, pad=(1, 1)),
+    BatchNorm(32, relu),
+    MaxPool((2,2)),
+    # 20x20 image
+    Conv((3, 3), 32=>32, pad=(1, 1)),
+    BatchNorm(32, relu),
+    MaxPool((2,2)),
+    # 10x10 image
+    Conv((3, 3), 32=>32, pad=(1, 1)),
+    BatchNorm(32, relu),
+    MaxPool((2,2)),
+    # 5x5 image
+    Conv((3, 3), 32=>1),
+    MeanPool((3, 3)),
+    flatten,
+    x->tanh.(x)
+    =#
+    flatten,
+    Dense(6400, 10, relu),
+    Dense(10,    1, tanh)
+)
+
+opt = ADAM()
+
+cnt = 1
+
+ps = params(model)
+trainX = Array{Float32}(undef, (80, 80, 1, 32))
+trainY = Array{Float32}(undef, (1, 32))
+
+for j=1:10
+    println("batch: ", j)
+    for i=1:32
+        th = (rand() - 5f-1)*2pi
+        cs  = CarState(car, bloc, goal, th)
+        img = signal(cs)
+        println(size(img))
+        trainX[:, :, 1, i] = img
+        trainY[1, i] = th
+    end
+
+    gs = gradient(ps) do
+        y = model(trainX)
+        return mse(y, trainY)
+    end
+    update!(opt, ps, gs)
+end
---
+++ Phase-2/Session-10/td3.jl
@@ -0,0 +1,132 @@
+using Flux
+using Random: rand
+using Flux: mse, cu
+import Flux: update!
+using StaticArrays
+
+mutable struct ReplayMemory{NR, NS, L}
+    m::MMatrix{L, NR, Float32}
+    idx::Int
+    n::Int
+    ReplayMemory{NR, NS, L}() where {NR, NS, L} =
+        new{NR, NS, L}(MMatrix{L, NR, Float32}(undef), 0, 0)
+end
+
+function Base.push!(mem::ReplayMemory{NR, NS},
+                    last_state, state, last_reward, last_action) where {NR, NS}
+    m, n, idx = mem.m, mem.n, mem.idx
+    n < NR && (n += 1)
+    idx = idx == NR ? 1 : idx+1
+
+    m[1:NS, idx]     .= last_state
+    m[NS+1:2NS, idx] .= state
+    m[end-1, idx] = last_reward
+    m[end, idx]   = last_action
+
+    mem.idx, mem.n = idx, n
+end
+
+function sample(mem::ReplayMemory{NR, NS}, sz::Int) where {NR, NS}
+    n, m = mem.n, mem.m
+    n < sz && error("Not enough data in memory for samples")
+    ids = rand(1:n, sz)
+    lstates = zeros(Float32, (NS, sz))
+    states  = zeros(Float32, (NS, sz))
+    rewards = zeros(Float32, (1,  sz))
+    actions = zeros(Int, sz)
+    for i = 1:sz
+        v = @view m[:, ids[i]]
+        lstates[:, i] .= v[1:NS]
+         states[:, i] .= v[NS+1:2NS]
+        rewards[1, i] = v[end-1]
+        actions[i] = Int(v[end])
+    end
+    return lstates, states, rewards, actions
+end
+
+
+mutable struct DQN{NS, NA}
+    model
+    optim
+    memory::ReplayMemory
+    gamma::Float32
+    last_action::Int
+    last_signal::Vector{Float32}
+    last_reward::Float32
+    trained::Bool
+    function DQN{NS, NA}(gamma) where {NS, NA}
+        model = Chain(Dense(NS, 16, relu),
+                      Dense(16, 16, relu),
+                      Dense(16, NA)) |> gpu
+        return new(model,
+                   ADAM(0.001, (0.9, 0.9)),
+                   ReplayMemory{100000, NS, 2NS+2}(),
+                   gamma,
+                   1, zeros(Float32, NS), 0f0,
+                   false)
+    end
+end
+
+function select_action(dqn::DQN{NS, NA}, s) where {NS, NA}
+    vals = softmax(dqn.model(s))
+    ix, v = 1, vals[1]
+    for i = 2:NA
+        if v < vals[i]
+            v = vals[i]
+            ix = i
+        end
+    end
+    return ix
+end
+
+function learn!(dqn::DQN{NS, NA}, csignals, nsignals, rewards, actionhb) where {NS, NA}
+    model, gamma = dqn.model, dqn.gamma
+    ps = params(model)
+    ones_arr = ones(Float32, (1, NA)) |> gpu
+    gamma_arr = fill(gamma, (1, NA+1)) |> gpu
+    gamma_arr[1, end] = 1f0
+    sz = size(rewards, 2)
+    mask = ones(Float32, (NA, sz)) |> cpu
+    for i=1:sz
+        if abs(rewards[1, i]-1f0) < 1f-6 || abs(rewards[1, i]+1f0) < 1f-6
+            mask[:, i] .= 0
+        end
+    end
+    mask = gpu(mask)
+    nouts = model(nsignals)
+    nouts = nouts.*softmax(nouts, dims=1)
+    nouts = mask.*nouts
+    nouts = vcat(nouts, rewards)
+    targets = gamma_arr*nouts
+    gs = gradient(ps) do
+        couts = model(csignals)
+        couts = couts.*actionhb
+        coutputs = ones_arr*couts
+        value = mse(targets, coutputs)
+        println("mse: ", value)
+        return value
+    end
+    update!(dqn.optim, ps, gs)
+end
+
+function update!(dqn::DQN{NS, NA}, reward, signal, episode=:c) where {NS, NA}
+    memory, last_signal, last_action = dqn.memory, dqn.last_signal, dqn.last_action
+
+    episode === :b ||
+        push!(memory, last_signal, signal, reward, last_action)
+
+    if dqn.memory.n > 128
+        csignals, nsignals, rewards, actions = sample(memory, 128) |> gpu
+        actionhb = Flux.onehotbatch(actions, 1:NA) |> gpu
+        learn!(dqn, csignals, nsignals, rewards, actionhb)
+    end
+    
+    action = rand() < 0.05 ? rand(1:NA) :
+        select_action(dqn, gpu(collect(signal)))
+
+    if episode !== :e
+        dqn.last_action = action
+        copyto!(last_signal, signal)
+    end
+    return action
+end
